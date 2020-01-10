ede5086f72a909e55b56778a8c8be7ada53c5818
wohlig <saraswati.gauda@wohlig.com>
2020-01-10 18:27:53 +0530

    1st git

---
+++ lib/drawer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountEmail: Text('sarasnmsp21@gmail.com'),
+              accountName: Text('Saraswati Gauda'),
+              currentAccountPicture: CircleAvatar(
+                child: Text('S'),
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text('Feature'),
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text('Setting'),
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text('Logout'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
